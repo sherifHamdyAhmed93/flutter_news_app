c84485d1999b68eaf7362ce86d2fc3717f04fcf5
Sherif Hamdy <shamdy@xapps.co>
2024-08-09 20:30:36 +0300

    Add app colors and theme.

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color primaryColor = Color(0xff39A552);
+  static Color whiteColor = Color(0xffffffff);
+  static Color categoriesTextColor = Color(0xff4F5A69);
+  static Color secondaryTextColor = Color(0xff79828B);
+  static Color primaryTextColor = Color(0xff42505C);
+  static Color sideMenuTextColor = Color(0xff303030);
+}
---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app/theme/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme{
+
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColors.primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(bottomRight: Radius.circular(40),bottomLeft: Radius.circular(40))
+      ),
+    ),
+    textTheme: TextTheme(
+      titleLarge: GoogleFonts.poppins(
+        fontSize: 22,
+        fontWeight: FontWeight.bold,
+        color: AppColors.categoriesTextColor
+      ),
+      titleMedium: GoogleFonts.exo(
+          fontSize: 22,
+          fontWeight: FontWeight.w400,
+          color: AppColors.whiteColor
+      ),
+      labelSmall: GoogleFonts.exo(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: AppColors.primaryColor
+      ),
+      headlineMedium: GoogleFonts.poppins(
+          fontSize: 14,
+          fontWeight: FontWeight.w600,
+          color: AppColors.primaryTextColor
+      ),
+      headlineSmall: GoogleFonts.poppins(
+          fontSize: 10,
+          fontWeight: FontWeight.w400,
+          color: AppColors.secondaryTextColor
+      ),
+    ),
+  );
+
+}
