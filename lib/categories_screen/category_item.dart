f610751965e7b62307d88eb32b5e1c54c4a837b3
Sherif Hamdy <shamdy@xapps.co>
2024-08-10 18:19:25 +0300

    Finish categories screen.

--- lib/categories_screen/category_item.dart
+++ lib/categories_screen/category_item.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/category_screen/category_screen.dart';
 import 'package:flutter_news_app/model/category_model.dart';
+import 'package:flutter_news_app/provider/source_provider.dart';
+import 'package:provider/provider.dart';
 
 class CategoryItem extends StatelessWidget {
   CategoryItem({super.key,required this.category});
@@ -8,18 +11,25 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: category.backgroundColor,
-          borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20),bottomRight: Radius.circular(20))
-      ),
-      padding: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          Expanded(child: Image.asset('assets/images/${category.imageName}')),
-          SizedBox(height: 10,),
-          Text('${category.name}',style: Theme.of(context).textTheme.titleMedium,)
-        ],
+    SourceProvider sourceProvider = Provider.of<SourceProvider>(context);
+    return InkWell(
+      onTap: (){
+        sourceProvider.setCategory(category);
+        Navigator.of(context).pushNamed(CategoryScreen.screenName,arguments: category);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: category.backgroundColor,
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20),bottomRight: Radius.circular(20))
+        ),
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Expanded(child: Image.asset('assets/images/${category.imageName}')),
+            SizedBox(height: 10,),
+            Text('${category.name}',style: Theme.of(context).textTheme.titleMedium,)
+          ],
+        ),
       ),
     );
   }
