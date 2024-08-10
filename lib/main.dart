64601cbe426d402bc6a0e41fcfe031581a91804d
Sherif Hamdy <shamdy@xapps.co>
2024-08-10 18:20:24 +0300

    Update main screen.

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_news_app/categories_screen/categories_screen.dart';
 import 'package:flutter_news_app/category_screen/category_screen.dart';
 import 'package:flutter_news_app/news_details/news_details_screen.dart';
+import 'package:flutter_news_app/provider/source_provider.dart';
 import 'package:flutter_news_app/theme/app_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SourceProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
